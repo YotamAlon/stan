7dac0e2c8d6fbcd7d54fafb9b0c607573dfe8d28
Yotam Alon <yotamalon@gmail.com>
2024-02-11 19:33:13 +0000

    Added basic logic for parsing packets (WIP)

--- stan.cpp
+++ stan.cpp
@@ -1,81 +1,85 @@
 #include <string>
 #include <iostream>
 #include <pcap.h>
+#include <unordered_map>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip6.h>
+#include <net/ethernet.h>
+#include <filesystem>
 
 using namespace std;
 
+struct Stats {
+    float kilobits;
+    int packets;
+    int connections;
+};
+
 int main(int argc, char *argv[]) {
-    /*
-    * Step 2 - Get a file name
-    */
- 
     string file = argv[1];
- 
-    /*
-    * Step 3 - Create an char array to hold the error.
-    */
+    std::filesystem::path cwd = std::filesystem::current_path();
+    std::unordered_map<std::string, std::string> stats_map;
  
     // Note: errbuf in pcap_open functions is assumed to be able to hold at least PCAP_ERRBUF_SIZE chars
     //       PCAP_ERRBUF_SIZE is defined as 256.
     // http://www.winpcap.org/docs/docs_40_2/html/group__wpcap__def.html
     char errbuff[PCAP_ERRBUF_SIZE];
  
-    /*
-    * Step 4 - Open the file and store result in pointer to pcap_t
-    */
- 
     // Use pcap_open_offline
     // http://www.winpcap.org/docs/docs_41b5/html/group__wpcapfunc.html#g91078168a13de8848df2b7b83d1f5b69
-    pcap_t * pcap = pcap_open_offline(file.c_str(), errbuff);
- 
-    /*
-    * Step 5 - Create a header and a data object
-    */
+    pcap_t* pcap = pcap_open_offline(file.c_str(), errbuff);
  
     // Create a header object:
     // http://www.winpcap.org/docs/docs_40_2/html/structpcap__pkthdr.html
     struct pcap_pkthdr *header;
  
-    // Create a character array using a u_char
-    // u_char is defined here:
-    // C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Include\WinSock2.h
-    // typedef unsigned char   u_char;
-    const u_char *data;
+    const u_char* data;
  
-    /*
-    * Step 6 - Loop through packets and print them to screen
-    */
     u_int packetCount = 0;
     while (int returnValue = pcap_next_ex(pcap, &header, &data) >= 0)
     {
-        // Print using printf. See printf reference:
-        // http://www.cplusplus.com/reference/clibrary/cstdio/printf/
- 
-        // Show the packet number
-        printf("Packet # %i\n", ++packetCount);
+
+        string address;
  
         // Show the size in bytes of the packet
-        printf("Packet size: %d bytes\n", header->len);
+        float bits = header->len * 8;
  
         // Show a warning if the length captured is different
         if (header->len != header->caplen)
             printf("Warning! Capture size different than packet size: %ld bytes\n", header->len);
  
         // Show Epoch Time
-        printf("Epoch Time: %d:%d seconds\n", header->ts.tv_sec, header->ts.tv_usec);
- 
-        // loop through the packet and print it as hexidecimal representations of octets
-        // We also have a function that does this similarly below: PrintData()
-        for (u_int i=0; (i < header->caplen ) ; i++)
-        {
-            // Start printing on the next after every 16 octets
-            if ( (i % 16) == 0) printf("\n");
- 
-            // Print each octet as hex (x), make sure there is always two characters (.2).
-            printf("%.2x ", data[i]);
+        // printf("Epoch Time: %d:%d seconds\n", header->ts.tv_sec, header->ts.tv_usec);
+        if (header->caplen < 8) {
+            continue;
         }
- 
-        // Add two lines between packets
-        printf("\n\n");
+
+        struct ether_header* eth_header;
+        eth_header = (struct ether_header*) data;
+        if (ntohs(eth_header->ether_type) == ETHERTYPE_IP) {
+            struct iphdr* ip_header;
+            ip_header = (struct iphdr*) (data + 8);
+            uint32_t sender = ip_header->saddr;
+            uint32_t receiver = ip_header->daddr;
+        }
+        // else if (ntohs(eth_header->ether_type) == ETHERTYPE_IPV6) {
+        //     struct ip6_hdr *ip_header;
+        //     ip_header = (struct ip6_hdr *) data[32];
+        // }
+    }
+};
+
+
+void print_packet(pcap_pkthdr *header, const u_char *data)
+{
+    for (u_int i = 0; (i < header->caplen); i++)
+    {
+        // Start printing on the next after every 16 octets
+        if ((i % 16) == 0)
+            printf("\n");
+
+        // Print each octet as hex (x), make sure there is always two characters (.2).
+        printf("%.2x ", data[i]);
     }
 }
